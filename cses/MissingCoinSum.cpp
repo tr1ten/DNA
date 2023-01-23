10518b9c9fe40414717e31485275035d85844533
tr1ten <shubhi.blog@gmail.com>
2023-01-23 07:00:01 +0530

    chore: Monday 23 January 2023 07:00:01 AM IST solutions

---
+++ cses/MissingCoinSum.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// useful defs
+typedef vector<int> VI;
+typedef long long LL; 
+typedef pair<int,int> PI;
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) VI name(sz,value)
+#define mk_mat(name,n,m,value) vector<vector<int>> name(n, vector<int>(m, value))
+#define contains(x) find(x) != string::npos
+#define take_vec(vec,sz) FOR(i,0,sz) cin>>vec[i]
+#define sort_vec(vec) sort(vec.begin(), vec.end())
+const LL MOD = 1e9+7;
+const int INF = 1e8+5;
+
+// actual solutions 
+size_t solve(size_t n,VI &coins){
+    sort_vec(coins);
+    LL sm = 0;
+    LL tar = 1;
+    trav(x,coins){
+        sm += x;
+        if(x==tar || sm==tar) tar = sm+1;
+        else return tar;
+    }
+    return n;
+}
+
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin>>N;
+    VI coins(N);
+    take_vec(coins,N);
+    cout << solve(N,coins);
+    return 0;
+}
