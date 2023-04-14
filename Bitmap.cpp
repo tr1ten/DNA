b4f2ab76d0529977ad5aab0ef3b82fc2cd5b00af
tr1ten <shubhi.blog@gmail.com>
2023-04-14 19:00:01 +0530

    chore: Friday 14 April 2023 07:00:01 PM IST solutions

---
+++ Bitmap.cpp
@@ -0,0 +1,103 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+typedef long long LL; 
+typedef vector<LL> VI;
+typedef vector<VI> VII;
+typedef pair<LL,LL> PI;
+typedef vector<PI> VPI;
+typedef unordered_map<LL,LL> MII;
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define F0R(i,a) FOR(i,0,a)
+#define rep(N) FOR(i,0,N)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) VI name(sz,value)
+#define mk_mat(name,n,m,value) vector<vector<int>> name(n, vector<int>(m, value))
+#define contains(x) find(x) != string::npos
+#define take_vec(vec,sz) FOR(i,0,sz) cin>>vec[i]
+#define sort_vec(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vec_sum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vec_max(vec) *max_element(vec.begin(), vec.end());
+#define vec_min(vec) *min_element(vec.begin(), vec.end());
+#define put_vec(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define take(x) cin >> x;
+#define take2(x,y) cin >> x >> y;
+#define take3(x,y,z) cin >> x >> y >> z;
+
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+const LL MOD = 1e9+7;
+const LL INF = 1e10+5;
+
+
+
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin>>T;
+    int dx[] = {0,0,-1,1};
+    int dy[] = {1,-1,0,0};
+    while(T--){
+        int n,m;
+        cin >> n >> m;
+        deque<pair<int,pair<int,int>>> dq;
+        int res[n][m];
+        memset(res,-1,sizeof res);
+        FOR(i,0,n){
+            string r ;
+            cin >> r;
+            FOR(j,0,m){
+                int x = r[j]-'0';
+                if(x) {
+                    dq.push_back({i,{j,0}});
+                    res[i][j] = 0;
+                }
+            }
+        }
+        while(!dq.empty()){
+            auto p = dq.front();
+            dq.pop_front();
+            FOR(i,0,4){
+                int y=p.first+dy[i],x=p.second.first+dx[i];
+                if(y<0 || y>=n || x<0 || x>=m || res[y][x]!=-1) continue;
+                res[y][x] = p.second.second+1;
+                dq.push_back({y,{x,p.second.second+1}});
+            }
+        }
+        FOR(i,0,n){
+            FOR(j,0,m){
+                cout << res[i][j] << " ";
+            }
+            cout <<endl;
+        }
+        
+    }
+
+    return 0;
+}
