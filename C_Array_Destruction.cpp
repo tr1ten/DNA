cb725c1ad9af098803032ee9fa4e10676037c019
tr1ten <shubhi.blog@gmail.com>
2023-07-05 19:00:01 +0530

    chore: Wednesday 05 July 2023 07:00:01 PM IST solutions

---
+++ C_Array_Destruction.cpp
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+typedef long long ll; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll> mll;
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vector<vi> name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+void __print(auto x) {cerr << x;}
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; __print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+const ll MOD = 1e9+7;
+const ll INF = 1e10+5;
+
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin >> n;
+        vi A(2*n);
+        tkv(A,2*n);
+        srv(A);
+        unordered_map<int,int> cnt;
+        trav(x,A) cnt[x]++;
+        int a;
+        int b;
+        int gf = 0;
+        rep(bi,0,2*n-1){
+            auto temp = cnt;
+            int tar =  A[bi] + A.back();
+            int init = tar;
+            vector<pair<int,int>> res;
+            per(i,0,2*n){
+                a=A[i];
+                b = tar-a;
+                if((a==b && (temp[a]<2)) || (a!=b && (temp[a]<1 || temp[b]<1)) ) continue;
+                temp[a]--;
+                temp[b]--;
+                res.push_back(make_pair(a,b));
+                tar = max(a,b);
+            }
+            if(res.size()==n){
+                gf = 1;
+                put("YES")
+                put(init)
+                trav(x,res) {
+                    put2(x.first,x.second);
+                }
+                break;
+            }
+        }
+        if(!gf) put("NO");
+
+    }
+
+    return 0;
+}
---
+++ F_1_Survival_of_the_Weakest_easy_version.cpp
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+typedef long long ll; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll> mll;
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vector<vi> name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) ((x)%MOD + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+void __print(auto x) {cerr << x;}
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; __print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+const ll MOD = 1e9+7;
+const ll INF = 1e10+5;
+
+vi F(vi A){
+    vi res;
+    srv(A);
+    auto gk = [&](const pair<int,int> p){
+        return (A[p.first]+A[p.second]);
+    };
+    auto cmp = [&](const pair<int,int> a,const pair<int,int> b){
+        return gk(a)>gk(b);
+    };
+    priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)> q(cmp);
+    q.push({0,0});
+    unordered_set<string> st;
+    while(!q.empty() && res.size()<A.size()-1) {
+        auto p = q.top();
+        q.pop();
+        string key = to_string(min(p.first,p.second))+ ","+ to_string(max(p.first,p.second));
+        if(p.first!=p.second && st.count(key)==0){st.insert(key); res.push_back((gk(p)));}
+        if(p.first==0 && p.second+1<A.size()) q.push({p.first,p.second+1});
+        if(p.first+1<A.size()) q.push({p.first+1,p.second});
+    }
+    return res;
+}
+ll fast_pow(ll a,ll b){
+    if(b==0) return 1;
+    if(b==1) return a;
+    ll res = fast_pow(a,b/2);
+    if(b%2==0) return (res*res)%MOD;
+    return (((res*res)%MOD)*a)%MOD;
+}
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T=1;
+    // cin>>T;
+    while(T--){
+        int n;
+        cin >> n;
+        vi A(n);
+        tkv(A,n);
+        ll x;
+        ll ex = 0;
+        rep(i,0,n-1){
+            x = *min_element(all(A));
+            rep(i,0,A.size()) A[i] -=x;
+            A = F(A);
+            ex = mod(ex+mod(x*(fast_pow(2,A.size()) ))  );
+        }
+        put(mod(A.back()+ex) );
+    }
+
+    return 0;
+}
