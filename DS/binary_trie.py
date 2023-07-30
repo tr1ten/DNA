bee7c922c0d5e2244a84527309feb896a60a309e
tr1ten <shubhi.blog@gmail.com>
2023-07-30 19:00:01 +0530

    chore: Sunday 30 July 2023 07:00:01 PM IST solutions

--- DS/binary_trie.py
+++ DS/binary_trie.py
@@ -1,10 +1,11 @@
 A = 2
-MX = 32;
+MX = 3;
 class Node:
     def __init__(self) -> None:
         self.childs = [None]*A
         self.ended = False
         self.val = -1
+        self.cnt = 0
 root = Node()
 def add(x):
     cur = root
@@ -13,8 +14,22 @@ def add(x):
         if not cur.childs[ind]: cur.childs[ind] = Node()
         cur = cur.childs[ind]
     cur.ended = 1;
+    cur.cnt+=1
     cur.val = x
 
+def remove(x):
+    cur = root
+    path = []
+    for i in range(MX,-1,-1): # store from msb...lsb
+        ind = (x>>i)&1
+        path.append((cur,ind))
+        cur = cur.childs[ind]
+    cur.cnt -=1
+    if cur.cnt==0:
+        while path:
+            p,i = path.pop()
+            p.childs[i] = None
+            if p.childs[i^1]: break 
 """
 Find max xor of x in trie
 """
@@ -29,7 +44,12 @@ def max_xor(x):
 def main():
     A = [8,12,9]
     for x in A: add(x)
-    print(max_xor(10))
+    print(max_xor(10)) # 12
+    remove(12)
+    print(max_xor(10)) # 12
+    remove(9)
+    print(max_xor(10)) # 12
+    
 
 if __name__=='__main__':
     main();
