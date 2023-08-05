512229fb4f32f9dc4f709c32bafc8dd5847d5a28
tr1ten <shubhi.blog@gmail.com>
2023-08-05 07:00:01 +0530

    chore: Saturday 05 August 2023 07:00:01 AM IST solutions

---
+++ A.java
@@ -0,0 +1,8 @@
+class A{
+    public static void main(String[] args) {
+        String s1 = "shubh";
+        String s2 = "shubh";
+        if(s1==s2) System.out.println("True");
+        else System.out.println("False");
+    }
+}
