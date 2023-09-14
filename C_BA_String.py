f45ce77f0fcde676f9f2a731f3ea5512e89f79f8
tr1ten <shubhi.blog@gmail.com>
2023-09-14 19:00:01 +0530

    chore: Thursday 14 September 2023 07:00:01 PM IST solutions

--- C_BA_String.py
+++ C_BA_String.py
@@ -10,33 +10,50 @@ def solve():
     a = []
     b = []
     i = 0
+    M = 1e19
     while i<n:
         if s[i]=='a': 
-            cs = ""
-            while i<len(s) and s[i]=='a': 
-                cs +='a'
-                i+=1
-            b.append(cs)
+            i +=1
             continue
         else:
-            len = 0
+            ck = 0
             while i<len(s) and s[i]=='*':
-                len +=1;
+                ck +=1;
                 i+=1
-            a.append(len*k)
+            a.append(min(M,ck*k))
     b.append("")
     suff = [1]
     for i in range(len(a)-1,-1,-1):
-        suff.append(suff[-1]*a[i])
-    ans = ""
+        suff.append(suff[-1]*(a[i]+1))
+    suff = suff[::-1]
+    js = []
+    # print(suff,a)
     for i in range(len(a)):
         j = 0
-        while suff[i+1]<=x:
+        cur = ""
+        while suff[i+1]<x:
+            j +=1 
+            cur += 'b'
+            x -=(suff[i+1])
+        assert(j<=a[i])
+        js.append(cur)
+        # print(ans,x,suff[i+1])
+    
+    ans = ""
+    j = 0
+    i =0
+    while i<n:
+        if s[i]=='a':
+            ans += 'a' 
+            i +=1
+            continue
+        else:
+            while i<len(s) and s[i]=='*':
+                i+=1
+            ans+=js[j]
             j +=1
-            ans += 'b'
-            x -=suff[i+1]
-        ans += b[i]
-        
+            a.append(ck*k)
+    print(ans)        
             
 # input processing
 for _ in range(t):
---
+++ D_Integers_Have_Friends.cpp
@@ -0,0 +1,129 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+typedef long long ll; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll> mll;
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+const ll MOD = 1e9+7;
+const ll INF = 1e10+5;
+
+const int MAXN = 1e5 + 5;
+const int MAXLOG = 25;
+int n, q;
+vi a;
+int st[MAXN][MAXLOG];
+
+void buildSparseTable() {
+    for (int i = 0; i < n; i++)
+        st[i][0] = a[i];
+    for (int j = 1; (1 << j) <= n; j++)
+        for (int i = 0; i + (1 << j) <= n; i++)
+            st[i][j] = __gcd(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+}
+
+int rangeGCD(int l, int r) {
+    int k = log2(r - l + 1);
+    return __gcd(st[l][k], st[r - (1 << k) + 1][k]);
+}
+
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);	  
+    int T=1;
+    cin>>T;
+    while(T--){
+        int N;
+        cin >> N;
+        vi b;
+        b.resize(n);
+        tkv(b,n);
+        a.resize(n-1);
+        n = N-1;
+        rep(i,1,n){
+            a[i-1] = abs(b[i]-b[i-1]);
+        }
+        buildSparseTable();
+        int i =0;
+        int res = 0;
+        rep(j,0,n)
+        {
+            if(i<j && rangeGCD(i,j)<=1) i++;
+            res += max(res,j-i+2);
+        }
+        put(res);
+        
+
+    }
+
+    return 0;
+}
