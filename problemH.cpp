f855a3b236b43d63880f985cd3c960e4150e3fbb
tr1ten <shubhi.blog@gmail.com>
2023-11-11 19:00:01 +0530

    chore: Saturday 11 November 2023 07:00:01 PM IST solutions

--- problemH.cpp
+++ problemH.cpp
@@ -1,207 +1,87 @@
-#include <cstdio>
-#include <bits/stdc++.h>
-
-using namespace std;
-#include "ext/pb_ds/assoc_container.hpp"
-#include "ext/pb_ds/tree_policy.hpp"
-using namespace __gnu_pbds;
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
-template<typename T> 
-using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
-// find_by_order(k)  returns iterator to kth element starting from 0;
-// order_of_key(k) returns count of elements strictly smaller than k;
-// useful defs
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(int x) const { return x ^ RANDOM; }
-};
-// gp_hash_table<int, int, chash> table;
-template <class K, class V>
-
-using ht = gp_hash_table<
-
-    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
-
-    hash_standard_resize_policy<hash_exponential_size_policy<>,
-
-                                hash_load_check_resize_trigger<>, true>>;
-
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-// ht<int, null_type> g;
-
-typedef long long ll; 
-typedef vector<ll> vi;
-typedef vector<vi> vii;
-typedef pair<ll,ll> pi;
-typedef vector<pi> vpi;
-typedef unordered_map<ll,ll,custom_hash> mll;
-#define pb push_back
-#define mp make_pair
-#define rep(i,a,b) for (int i = (a); i < (b); i++)
-#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
-#define trav(a,arr) for (auto& a: (arr))
-#define sz(x) (int)(x).size()
-#define mk_vec(name,sz,value) vi name(sz,value)
-#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
-#define contains(x) find(x) != string::npos
-#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
-#define srv(vec) sort(vec.begin(), vec.end())
-#define all(x) x.begin(), x.end()
-#define less(a,b) a<b
-#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
-#define vmax(vec) *max_element(vec.begin(), vec.end());
-#define vmin(vec) *min_element(vec.begin(), vec.end());
-#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
-#define put(x) cout<<(x)<<endl;
-#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
-#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
-#define mod(x) (x + MOD)%MOD
-// debugging
-#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
-
-
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
-const ll MOD = 1e9+7;
-const ll INF = 1e10+5;
-
-inline int ctz(ll x) { return __builtin_ctzll(x);}
-inline int clz(ll x) {return __builtin_clzll(x);}
-inline int pc(ll x) {return  __builtin_popcount(x);} 
-inline int hset(ll x) {return __lg(x);}
-void ans(int x) {put(x?"YES":"NO");}
-#include <bits/stdc++.h>
-using namespace std;
-// battle tested
-typedef long long ll;
-const int N = 2*(1e5) + 5;
-int n;
-struct Segment{
-    int sum=0,
-    pref1=0,
-    suff0=0;
-} segm[4*N]; // 0 index
-
-Segment combine(const Segment &left,const Segment &right){
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std; 
+const int SZ = 262144; 
+struct Segment 
+{
+    int sum=0;
+    int suff0 = 0;
+    int pref1 = 0;
+}seg[2*SZ];
+Segment combine(Segment left, Segment right)
+{
     Segment res;
     res.sum = (left.sum + right.sum);
     int d = min(left.suff0,right.pref1);
-    res.pref1 = left.pref1 + right.pref1-d;
-    res.suff0 = right.suff0 + left.suff0-d;
+    res.pref1 = left.pref1 ;
+    res.suff0 = right.suff0;
+    if(d==left.suff0) res.pref1 += right.pref1-d;
+    if(d==right.pref1) res.suff0 += left.suff0 -d;
     res.sum +=d;
-    // debug(left,right,res);
+    return res;
     return res;
 }
-// child- 2*x+1,2*x+2 (0 coz index)
-// add x to [l...r]
-void update(int node,int left,int right,int i,ll value){
-    if(left==i && right==i) {
-            if(value) segm[node].pref1 = 1;
-            else segm[node].suff0 = 1;
-        }
-    else{
-        int mid = (left+right)/2;
-        if(i<=mid) update(2*node+1,left,mid,i,value);
-        else update(2*node+2,mid+1,right,i,value);
-        segm[node] = combine(segm[2*node+1] , segm[2*node+2]);
-
+int update(int p, int value) 
+{
+    p += SZ;
+    seg[p].suff0 = !value;
+    seg[p].pref1 = value;
+    for (; p > 1; p >>= 1)
+    {
+            seg[p>>1] = combine(seg[(p|1)^1], seg[p|1]);
     }
-    
+    return 0;
+} 
+Segment query(int l, int r) 
+{ 
+    Segment resL, resR; r++;
+    for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
+        if (l&1) resL = combine(resL,seg[l++]);
+        if (r&1) resR = combine(seg[--r],resR);
+    }
+    return combine(resL,resR);
 }
-
-Segment query(int node,int left,int right,int l,int r){
-    if(left>=l && right<=r) {return segm[node];}
-    int mid = (left+right)/2;
-    Segment lf,rf;
-    if(l<=mid) lf = query(2*node+1,left,mid,l,r);
-    if(r>mid) rf = query(2*node+2,mid+1,right,l,r);
-
-    return combine(lf,rf);
-}
-// everything is zero indexed
-void update(int i,ll x){
-    update(0,0,n-1,i,x);
-}
-Segment query(int l,int r){
-    return query(0,0,n-1,l,r);
-}
-void testcase(){
+int solve()
+{
     string s;
-    cin >> s;
-    n = s.size();
-    rep(i,0,n){
-        update(i,s[i]-'0');
+    cin>>s;
+    int n = s.size();
+    for(int i=0; i<n; i++)
+    {
+        update(i, s[i]-'0');
     }
-    int q;
-    cin >> q;
-    rep(i,0,q){
-        int k;
-        cin >> k;
-        if(k==1){
-            int x;
-            cin >> x;
-            --x;
-            if(s[x]=='1') s[x] = '0';
-            else s[x] = '1';
-            update(i,s[x]-'0');
+    int q; cin>>q;
+    while(q--)
+    {
+        int type;
+        cin>>type;
+        if(type==1)
+        {
+            int pos;
+            cin>>pos; pos--;
+            update(pos, (s[pos]-'0')^1);
+            s[pos] = '0' + ((s[pos]-'0')^1);
         }
-        else{
-            int l,r;
-            cin >> l >> r;
-            --r;--l;
-            put(r-l+1 - 2*query(l,r).sum);
+        else
+        {
+            int l, r;
+            cin>>l>>r;
+            l--; r--;
+            Segment ans = query(l, r);
+            cout<<(r-l+1-2*ans.sum)<<endl;    
         }
     }
-    
+    return 0;
 }
-// driver code
 int main()
 {
-    ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-    // freopen("input.in","r",stdin);
-    // freopen("output.out","w",stdout);      
-    int T=1;
-    // cin>>T;
-    while(T--) testcase();
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
     return 0;
 }
