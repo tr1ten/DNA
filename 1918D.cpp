621b419f643810cb64ef9ce49ec4e701b2de6971
tr1ten <shubhi.blog@gmail.com>
2024-02-02 07:00:01 +0530

    chore: Friday 02 February 2024 07:00:01 AM IST solutions

--- 1918D.cpp
+++ 1918D.cpp
@@ -109,36 +109,90 @@ inline int clz(ll x) {return __builtin_clzll(x);}
 inline int pc(ll x) {return  __builtin_popcount(x);} 
 inline int hset(ll x) {return __lg(x);}
 void pyn(int x) {put(x?"YES":"NO");}
+
+#include <bits/stdc++.h>
+using namespace std;
+// battle tested
+// https://judge.yosupo.jp/submission/170986
+typedef long long ll;
+const int N = 5*(1e5) + 5;
+int n;
+struct Segment{
+    ll val=INF;
+} segm[4*N]; // 0 index
+Segment combine(Segment left,Segment right){
+    Segment res;
+    res.val = min(left.val,right.val);
+    return res;
+}
+
+// child- 2*x+1,2*x+2 (0 coz index)
+// add x to [l...r]
+void update(int node,int left,int right,int i,ll value){
+    if(left==i && right==i) {
+            segm[node].val = value;
+        }
+    else{
+        int mid = (left+right)/2;
+        if(i<=mid) update(2*node+1,left,mid,i,value);
+        else update(2*node+2,mid+1,right,i,value);
+        segm[node] = combine(segm[2*node+1] , segm[2*node+2]);
+    }
+    
+}
+
+Segment query(int node,int left,int right,int l,int r){
+    if(left>=l && right<=r) {return segm[node];}
+    int mid = (left+right)/2;
+    Segment res;
+    if(l<=mid) res = combine(res,query(2*node+1,left,mid,l,r));
+    if(r>mid) res = combine(res,query(2*node+2,mid+1,right,l,r));
+
+    return res;
+}
+// everything is zero indexed
+void update(int i,ll x){
+    update(0,0,n-1,i,x);
+}
+Segment query(int l,int r){
+    return query(0,0,n-1,l,r);
+}
+
 // do not use unordered map use mll
 void testcase(){
     int n;
     cin >> n;
     vi a(n);
     tkv(a,n);
+    a.push_back(0);
+    n++;
+    vi pref(n+1);
+    rep(i,0,n){
+        pref[i+1] = pref[i] + a[i];
+    }
+    debug(pref);
     auto f = [&](ll x){
-        ll sm = 0;
-        ll bl = 0;
-        int j = 0;
-        rep(i,0,n){
-            if(a[i]>x) return false;
-            if(sm+a[i]>x){
-                if(a[j]<a[i] && sm-a[j]+a[i]<=x){
-                    sm +=a[i]-a[j];
-                    bl +=a[j];
-                    j++;
-                    continue;
-                }
-                bl += a[i];
-                j=i+1;
-                if(bl>x) return false;
-                sm = 0;
-            }
-            else{
-            sm +=a[i];
-            }
+        vi dp(n+1,INF);// dp[i] - min sum of blocked el st valid and ith element is blocked
+        rep(i,0,n+1){
+            segm[i] = {
+                INF
+            };
         }
-        // debug(bl,sm,x);
-        return bl<=x && sm<=x;
+        dp[0] = 0;
+        rep(i,1,n+1){
+            int j = lower_bound(all(pref),pref[i-1]-x)-pref.begin();
+            dp[i] = query(j,i-1).val + a[i-1];
+            // per(j,0,i){
+            //     if(pref[i-1]-pref[j]>x) {
+            //         break;
+            //     }
+            //     dp[i] = min(dp[j] + a[i-1],dp[i]);
+            //     // debug(i,j,x,pref[i]-pref[j]);
+            // }
+            // update(i,dp[i]);
+            debug(i,dp[i]);
+        }
+        return dp.back() <= x;
     };
     ll lo = 0,hi = 1e15;
     ll ans = 0;
@@ -154,7 +208,6 @@ void testcase(){
         }
     }
     put(ans);
-    
     
 }
 // driver code
