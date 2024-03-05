16f10708c0c2014d02f41d0a6c72ebf51ef97e5f
tr1ten <shubhi.blog@gmail.com>
2024-03-05 19:00:01 +0530

    chore: Tuesday 05 March 2024 07:00:01 PM IST solutions

---
+++ Matrix_Modulo.cpp
@@ -0,0 +1,209 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+// gp_hash_table<int, int, chash> table;
+template <class K, class V>
+
+using ht = gp_hash_table<
+
+    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
+
+    hash_standard_resize_policy<hash_exponential_size_policy<>,
+
+                                hash_load_check_resize_trigger<>, true>>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+// ht<int, null_type> g;
+
+typedef long long ll; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll,custom_hash> mll;
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+const ll MOD = 1e9+7; // change me for god sake look at problem mod
+const ll INF = 1e10+5;
+
+inline int ctz(ll x) { return __builtin_ctzll(x);}
+inline int clz(ll x) {return __builtin_clzll(x);}
+inline int pc(ll x) {return  __builtin_popcount(x);} 
+inline int hset(ll x) {return __lg(x);}
+void pyn(int x) {put(x?"YES":"NO");}
+
+
+
+const int N = 5*(1e5) + 5;
+long long fact[N];
+
+void pre() {
+    fact[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        fact[i] = (fact[i-1]*i)%MOD;
+    }
+}
+
+
+// do not use unordered map use mll
+int n;
+int dg;
+int lf;
+int check(vii &mat){
+    unordered_set<ll> used;
+    ll mn = n*(n+1)/2;
+    rep(i,0,n){
+        rep(j,i,n){
+            if(used.find(mat[i][j])!=used.end()) return 0;
+            if(mat[i][j]==0 && mat[j][i]==0) continue;
+            if(mat[i][j] && mat[j][i] && abs(mat[i][j] - mat[j][i])%mn!=0 ){
+                return 0;
+            }
+            if(i==j){
+                if(!(mat[i][j]>(n*(n-1)/2) && mat[i][j]<=n*(n+1)/2)) return 0;
+                used.insert(mat[i][j]);
+                dg--;
+                continue;
+            }
+            ll tar=0;
+            if(mat[i][j]){
+                tar = (mn+mat[i][j]) > n*n ? mat[i][j] - mn : mat[i][j] + mn;
+                mat[j][i] = tar;
+                used.insert(mat[i][j]);
+            }
+            else{
+                tar = (mn+mat[j][i]) > n*n ? mat[j][i] - mn : mat[j][i] + mn;
+                mat[i][j] = tar;
+                used.insert(mat[j][i]);
+
+            }
+            if(tar>0 && tar<=n*n && used.find(tar)==used.end()) {
+                used.insert(tar);
+            }
+            else { return 0;}
+        }
+    }
+   
+    lf = n*n -used.size();
+    return 1;
+}
+long long fast_pow(long long a,long long b,long long MOD){
+    if(b==0) return 1;
+    long long res = fast_pow(a,b/2,MOD);
+    if(b%2==0) return (res*res)%MOD;
+    return (((res*res)%MOD)*a)%MOD;
+}
+void testcase(){
+    cin >> n;
+    vii a(n,vi(n));
+    rep(i,0,n){
+        rep(j,0,n){
+            cin >> a[i][j];
+        }
+    }
+
+    dg = n;
+    lf = 0;
+    if(!check(a)) {put(0);}
+    else{   
+        assert((lf-dg)%2==0);
+        int prs = (lf-dg)/2;
+        assert(prs>=0 && dg>=0);
+        ll a1 = fact[dg];
+        ll a2 =mod(fast_pow(2,prs,MOD)*(fact[prs]));
+        put(mod(a1*a2));
+    }
+
+}
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    pre();
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);      
+    int T=1;
+    cin>>T;
+    while(T--) testcase();
+
+    return 0;
+}
