fcaaff0b2c1664590fc04b264b1c6c2f48a87e1c
tr1ten <shubhi.blog@gmail.com>
2024-04-16 07:00:01 +0530

    chore: Tuesday 16 April 2024 07:00:01 AM IST solutions

---
+++ codeforces/expert/1955/H.cpp
@@ -0,0 +1,189 @@
+// Problem: H. The Most Reckless Defense
+// Contest: Codeforces - Codeforces Round 938 (Div. 3)
+// URL: https://codeforces.com/contest/1955/problem/H
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+// gp_hash_table<int, int, chash> table;
+template <class K, class V>
+
+using ht = gp_hash_table<
+
+    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
+
+    hash_standard_resize_policy<hash_exponential_size_policy<>,
+
+                                hash_load_check_resize_trigger<>, true>>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+// ht<int, null_type> g;
+
+typedef long long ll; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll,custom_hash> mll;
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+const ll MOD = 1e9+7; // change me for god sake look at problem mod
+const ll INF = 1e16+5;
+
+inline int ctz(ll x) { return __builtin_ctzll(x);}
+inline int clz(ll x) {return __builtin_clzll(x);}
+inline int pc(ll x) {return  __builtin_popcount(x);} 
+inline int hset(ll x) {return __lg(x);}
+void pyn(int x) {put(x?"YES":"NO");}
+// do not use unordered map use mll
+const int N=55;
+char mat[N][N];
+struct T{
+	int i,j,p;
+};
+const int M = 12;
+const int K = 2505;
+ll dp[K][1<<(M)];
+
+int n,m,k;
+ll go(int i,ll mask, vii &val) {
+	if(i==k) return 0;
+	if(dp[i][mask]!=-1) return dp[i][mask];
+	ll res = go(i+1,mask,val);
+	rep(j,0,M){
+		if(mask&(1<<j)) continue;
+		res = max(res,val[i][j] + go(i+1,mask | 1<<j,val));
+	}
+	return dp[i][mask] = res;
+}
+void testcase(){
+	cin >> n>> m >> k;
+	vpi mm;
+	vpi ss;
+	rep(i,0,n){
+		rep(j,0,m){
+			cin >> mat[i][j];
+			if(mat[i][j]=='#') mm.pb({i,j});
+		}
+	}
+	ll ans = 0;
+	vii mat2(k,vi(M));
+	rep(i,0,k){
+		T st;
+		cin >> st.i  >> st.j >> st.p;	
+		st.i--;st.j--;
+		rep(r,1,M+1) {
+			ll x=0;
+			trav(pp,mm) {
+				ll dist = (pp.first-st.i)*(pp.first-st.i) + (pp.second-st.j)*(pp.second-st.j);
+				if(dist <= r*r) {
+					x++;
+				}
+			}
+			ll pr = x*st.p - (ll)pow(3,r);
+			mat2[i][r-1] = pr;
+		}
+	}
+	rep(i,0,k+1) {
+		memset(dp[i],-1,sizeof dp[i]);
+	}
+	put(go(0,0,mat2));
+	
+	
+	
+}
+// driver code
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);      
+    int T=1;
+    cin>>T;
+    while(T--) testcase();
+
+    return 0;
+}
