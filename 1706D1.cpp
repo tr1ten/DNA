635907571b77e9c12f7a1372a1c5d422f658ad58
tr1ten <shubhi.blog@gmail.com>
2024-05-03 19:13:50 +0530

    chore: Friday 03 May 2024 07:13:50 PM IST solutions

---
+++ 1706D1.cpp
@@ -0,0 +1,185 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+// gp_hash_table<int, int, chash> table;
+template <class K, class V>
+
+using ht = gp_hash_table<
+
+    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
+
+    hash_standard_resize_policy<hash_exponential_size_policy<>,
+
+                                hash_load_check_resize_trigger<>, true>>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+// ht<int, null_type> g;
+
+typedef long long ll; 
+typedef unsigned long long ull; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll,custom_hash> mll;
+#define pb push_back
+#define mp make_pair
+#define ss second
+#define ff first
+#define int long long
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+const ll MOD = 1e9+7; // change me for god sake look at problem mod
+const ll INF = 1e16+5;
+
+inline int ctz(ll x) { return __builtin_ctzll(x);}
+inline int clz(ll x) {return __builtin_clzll(x);}
+inline int pc(ll x) {return  __builtin_popcount(x);} 
+inline int hset(ll x) {return __lg(x);}
+void pyn(int x) {put(x?"YES":"NO");}
+// do not use unordered map use mll
+
+// 
+// for each k
+//  z = a/(max/k)  
+
+// max-min
+
+// mx/p1 - mn/p2 
+// mn .... mx
+
+// a1 a2 a3 a4
+// [a1/k,a1/(k-1),...,a1]
+
+// a1: b1 b2
+// a2: b3 b4
+// a3: b5 b6
+// a4: b7 b8
+
+// a/k - a/(k-1) = (ak - a - ak)/k*(K-1)
+
+// a/k*(k-1)
+
+struct payer{
+    int val;
+    int kk;
+    int ind;
+};
+bool operator<(const payer a,const payer b){
+    return a.val==b.val ? a.ind<b.ind : a.val < b.val;
+}
+void testcase(){
+    int n,k;
+    cin >> n >> k;
+    set<payer> st;
+    vi a(n);
+    tkv(a,n);
+    rep(i,0,n) {
+        payer s{a[i]/k,k,i};
+        st.insert(s);   
+    }
+    int res = INF;
+    while (1)
+    {
+        payer start = *(st.begin());
+        payer end = *(st.rbegin());
+        res = min(res,end.val-start.val);
+        if(start.kk==1) break;
+        else {
+            st.erase(start);
+            start.kk--;
+            start.val = a[start.ind]/(start.kk);
+            st.insert(start);
+        }
+    }
+    put(res);
+    
+}
+// driver code
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);      
+    int T=1;
+    cin>>T;
+    while(T--) testcase();
+
+    return 0;
+}
