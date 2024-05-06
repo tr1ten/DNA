f733acde532ce537521f5a125bc0b0c1f3a4c824
tr1ten <shubhi.blog@gmail.com>
2024-05-06 19:00:01 +0530

    chore: Monday 06 May 2024 07:00:01 PM IST solutions

---
+++ 1702G2.cpp
@@ -0,0 +1,230 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+// useful defs
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+// gp_hash_table<int, int, chash> table;
+template <class K, class V>
+
+using ht = gp_hash_table<
+
+    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
+
+    hash_standard_resize_policy<hash_exponential_size_policy<>,
+
+                                hash_load_check_resize_trigger<>, true>>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+// ht<int, null_type> g;
+
+typedef long long ll; 
+typedef unsigned long long ull; 
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vpi;
+typedef unordered_map<ll,ll,custom_hash> mll;
+#define pb push_back
+#define mp make_pair
+#define ss second
+#define ff first
+#define int long long
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define trav(a,arr) for (auto& a: (arr))
+#define sz(x) (int)(x).size()
+#define mk_vec(name,sz,value) vi name(sz,value)
+#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
+#define contains(x) find(x) != string::npos
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define less(a,b) a<b
+#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
+#define vmax(vec) *max_element(vec.begin(), vec.end());
+#define vmin(vec) *min_element(vec.begin(), vec.end());
+#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout<<(x)<<endl;
+#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
+#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
+#define mod(x) (x + MOD)%MOD
+// debugging
+#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
+
+
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+const ll MOD = 1e9+7; // change me for god sake look at problem mod
+const ll INF = 1e16+5;
+
+inline int ctz(ll x) { return __builtin_ctzll(x);}
+inline int clz(ll x) {return __builtin_clzll(x);}
+inline int pc(ll x) {return  __builtin_popcount(x);} 
+inline int hset(ll x) {return __lg(x);}
+void pyn(int x) {put(x?"YES":"NO");}
+
+// property of path in tree?
+
+
+int n, l; // nodes, logn
+vector<vector<int>> adj;
+int timer;
+vector<int> tin, tout;
+vector<vector<int>> up;
+vi depth;
+void dfs(int v, int p)
+{
+    if(p!=v) {
+        depth[v] = depth[p]+1;
+    }
+    tin[v] = ++timer;
+    up[v][0] = p;
+    for (int i = 1; i <= l; ++i)
+        up[v][i] = up[up[v][i-1]][i-1];
+
+    for (int u : adj[v]) {
+        if (u != p){
+            dfs(u, v);
+            }
+    }
+
+    tout[v] = ++timer;
+}
+
+bool is_ancestor(int u, int v)
+{
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+
+int lca(int u, int v) // find lowest common ancestor of u and v
+{
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
+    for (int i = l; i >= 0; --i) {
+        if (!is_ancestor(up[u][i], v)) // similar to exhaustive binary search 
+            u = up[u][i];
+    }
+    return up[u][0];
+}
+int path_dist(int u,int v){
+    int lc = lca(u,v);
+    return depth[u]+depth[v] -2*depth[lc];
+}
+// must call this function before using lca
+void preprocess(int root) {
+    tin.resize(n);
+    tout.resize(n);
+    depth.resize(n);
+    timer = 0;
+    l = ceil(log2(n));
+    up.assign(n, vector<int>(l + 1));
+    dfs(root, root);
+}
+
+
+// do not use unordered map use mll
+void testcase(){
+    cin  >> n;
+    adj.resize(n);
+    rep(i,0,n-1){
+        int u,v;
+        cin >> u >> v;
+        --u;--v;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    preprocess(0);
+    int q;
+    cin>> q;
+    rep(i,0,q){
+        int k;
+        cin >> k;
+        vi qs(k);
+        rep(j,0,k) {cin >> qs[j];qs[j]--;}
+        int lc = (qs[0]);
+        int fst=qs[0];
+        trav(j,qs){
+            lc = lca(j,lc);
+            if(depth[fst]<depth[j]) fst=j;
+        }
+        int scd=-1;
+        trav(j,qs){
+            if(lca(fst,j)!=j && (scd==-1 || depth[scd]<depth[j])) scd = j;
+        }
+        if(scd==-1) {pyn(1);continue;}
+        int third=-1;
+        trav(j,qs){
+            if(lca(fst,j)!=j && lca(scd,j)!=j) third = j;
+        }
+        if(third!=-1) {pyn(0);continue;}
+        pyn((path_dist(fst,scd)==path_dist(fst,lc) + path_dist(scd,lc) ));
+
+
+    }
+    
+}
+// driver code
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);      
+    int T=1;
+    while(T--) testcase();
+
+    return 0;
+}
