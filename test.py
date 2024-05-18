26b29cd7ed1d7db63dba96995f8054f6116250ec
tr1ten <shubhi.blog@gmail.com>
2024-05-18 19:00:00 +0530

    chore: Saturday 18 May 2024 07:00:00 PM IST solutions

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-def isPrime(x):
-    for i in range(2,int(x**0.5) + 1):
-        if x%i==0:
-            return False
-    return True
-def isPali(x):
-    return x==x[::-1]
-class Solution:
-    def primePalindrome(self, n: int) -> int:
-        for i in range(n,10000000):
-            if isPali(str(i)) and isPrime(i):
-                print(i)
-        return 0
-spol = Solution().primePalindrome(2)
+def isP(x):
+    return len(str(x))%2==0 and str(x)==str(x)[::-1]
+cnt = 1
+for i in range(10**5):
+    if isP(i):
+        print(cnt,i)
+        cnt +=1 
