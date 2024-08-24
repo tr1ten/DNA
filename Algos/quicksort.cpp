f8165f6b67ba0d18c5ecba68bfd41d8a57161434
tr1ten <shubhi.blog@gmail.com>
2024-08-24 07:00:00 +0530

    chore: Saturday 24 August 2024 07:00:00 AM IST solutions

--- Algos/quicksort.cpp
+++ Algos/quicksort.cpp
@@ -1,80 +1,80 @@
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-void swap(vector<int> &arr,int i,int j)
-{
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-// Hoare's partition
-// left pointer track smaller than pivot right pointer bigger
-int hpartition(vector<int> &arr,int l,int r){
-    int left = l-1;
-    int right = r+1;
-    int pivot = arr[l];
-    while (true)
+    #include <iostream>
+    #include <vector>
+    #include <string>
+    using namespace std;
+    void swap(vector<int> &arr,int i,int j)
     {
-        do{
-            left++;
-        }
-        while (arr[left]<pivot); 
-        // no equality is important so that pivot moves to correct position       
-        do
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    // Hoare's partition
+    // left pointer track smaller than pivot right pointer bigger
+    int hpartition(vector<int> &arr,int l,int r){
+        int left = l-1;
+        int right = r+1;
+        int pivot = arr[l];
+        while (true)
         {
-            right--;
-        } 
-        while (arr[right]>pivot);
-        if(left>=right) return left;
-        swap(arr,left,right);
-    }
-    
-}
-// Lomuto partition scheme
-int partition(vector<int> &arr,int l,int r)
-{
-    // keep track of current smaller element than pivot
-    int j=l-1;
-    int pivot = arr[r];
-    // starting element less than pivot
-    // shift all smaller element than pivot to the left
-    for (int i = l; i < r; i++)
-    {
-        if(pivot>arr[i]){
-            j++;
-            swap(arr,i,j);
+            do{
+                left++;
+            }
+            while (arr[left]<pivot); 
+            // no equality is important so that pivot moves to correct position       
+            do
+            {
+                right--;
+            } 
+            while (arr[right]>pivot);
+            if(left>=right) return left;
+            swap(arr,left,right);
         }
+        
+    }
+    // Lomuto partition scheme
+    int partition(vector<int> &arr,int l,int r)
+    {
+        // keep track of current smaller element than pivot
+        int j=l-1;
+        int pivot = arr[r];
+        // starting element less than pivot
+        // shift all smaller element than pivot to the left
+        for (int i = l; i < r; i++)
+        {
+            if(pivot>arr[i]){
+                j++;
+                swap(arr,i,j);
+            }
+
+        }
+        // j is the last element smaller than pivot
+        swap(arr,j+1,r);
+        return j+1;
+        
+    }
+    void quicksort(vector<int> &arr,int l,int r)
+    {
+        if(l>=r)
+        {
+            return;
+        }
+        int pi = hpartition(arr,l,r);
+        quicksort(arr,l,pi-1);
+        quicksort(arr,pi+1,r);
 
     }
-    // j is the last element smaller than pivot
-    swap(arr,j+1,r);
-    return j+1;
-    
-}
-void quicksort(vector<int> &arr,int l,int r)
-{
-    if(l>=r)
+    void printArr(vector<int> arr)
     {
-        return;
+        for (auto i:arr)
+        {
+            cout << i << " ";
+        }
     }
-    int pi = hpartition(arr,l,r);
-    quicksort(arr,l,pi-1);
-    quicksort(arr,pi+1,r);
-
-}
-void printArr(vector<int> arr)
-{
-    for (auto i:arr)
+    int main()
     {
-        cout << i << " ";
+        vector<int> arr = {8,7,6,1,0,9,2};
+        quicksort(arr,0,6);
+        printArr(arr);
+        
+        return 0;
     }
-}
-int main()
-{
-    vector<int> arr = {8,7,6,1,0,9,2};
-    quicksort(arr,0,6);
-    printArr(arr);
-    
-    return 0;
-}
--- DS/heap.c
+++ DS/heap.c
@@ -1,112 +1,112 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-void printArr(int *arr, int start,int n)
-{
-    // printf("%d",arr[0]);
-    printf("\n[ ");
-    for (int i = start; i < n; i++)
+    void printArr(int *arr, int start,int n)
     {
-        printf(" %d ", arr[i]);
-    }
-    printf("]\n ");
-}
-
-void swap(int *arr,int i,int j)
-{
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-
-int heapify(int *heap, int n, int i)
-{
-    int largest = i;
-    int left = 2*i+1;
-    int right = left+1;
-    if(left<n&&heap[left]>heap[largest])
-    {
-        largest = left;
-    }
-    if(right<n&&heap[right]>heap[largest])
-    {
-        largest = right;
-    }
-    if(heap[largest]>heap[i])
-    {
-        swap(heap,largest,i);
-        heapify(heap,n,largest);
-    }
-
-}
-
-void increaseKey(int *heap,int n,int index,int key){
-    if(index>=n || heap[index]>=key) return;
-    heap[index] = key;
-    int i=index;
-    while (i>=0 && key>heap[(i-1)/2])
-    {
-        swap(heap,i,(i-1)/2);
-        i = (i-1)/2;
-    }
-    
-}
-
-// swap ith element with last element, now check whether its child is larger, swap with it.
-void deleteHeap(int *heap,int n,int i)
-{
-    if(heap[i]<heap[n-1]) {
-        return increaseKey(heap,n-1,i,heap[n-1]);
-    }
-    else{
-        heap[i] = heap[n-1];
-        heap[n-1] = 0;
-        heapify(heap,n-1,i);
-    }
-    
-}
-// insert into correct position by checking parent, swapping if needeed
-void insertHeap(int value,int *heap, int n)
-{
-    heap[n] = value;
-    int i = n;
-    while (i > 0)
-    {
-        if (value>heap[i/2])
+        // printf("%d",arr[0]);
+        printf("\n[ ");
+        for (int i = start; i < n; i++)
         {
-            swap(heap,i,i/2); 
+            printf(" %d ", arr[i]);
+        }
+        printf("]\n ");
+    }
+
+    void swap(int *arr,int i,int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    int heapify(int *heap, int n, int i)
+    {
+        int largest = i;
+        int left = 2*i+1;
+        int right = left+1;
+        if(left<n&&heap[left]>heap[largest])
+        {
+            largest = left;
+        }
+        if(right<n&&heap[right]>heap[largest])
+        {
+            largest = right;
+        }
+        if(heap[largest]>heap[i])
+        {
+            swap(heap,largest,i);
+            heapify(heap,n,largest);
+        }
+
+    }
+
+    void increaseKey(int *heap,int n,int index,int key){
+        if(index>=n || heap[index]>=key) return;
+        heap[index] = key;
+        int i=index;
+        while (i>=0 && key>heap[(i-1)/2])
+        {
+            swap(heap,i,(i-1)/2);
+            i = (i-1)/2;
+        }
+        
+    }
+
+    // swap ith element with last element, now check whether its child is larger, swap with it.
+    void deleteHeap(int *heap,int n,int i)
+    {
+        if(heap[i]<heap[n-1]) {
+            return increaseKey(heap,n-1,i,heap[n-1]);
         }
         else{
-            break;
+            heap[i] = heap[n-1];
+            heap[n-1] = 0;
+            heapify(heap,n-1,i);
         }
-        i /=2;
+        
     }
-}
-int buildHeap(int* heap,int n)
-{
-    for (int i = n/2-1; i >=0; i--)
+    // insert into correct position by checking parent, swapping if needeed
+    void insertHeap(int value,int *heap, int n)
     {
-        heapify(heap,n,i);
+        heap[n] = value;
+        int i = n;
+        while (i > 0)
+        {
+            if (value>heap[i/2])
+            {
+                swap(heap,i,i/2); 
+            }
+            else{
+                break;
+            }
+            i /=2;
+        }
     }
-    
-}
-int heapSort(int *heap,int n)
-{
-    buildHeap(heap,n);
-
-    for (int i = n-1; i >=0; i--)
+    int buildHeap(int* heap,int n)
     {
-        swap(heap,0,i);
-        heapify(heap,i,0);
-
+        for (int i = n/2-1; i >=0; i--)
+        {
+            heapify(heap,n,i);
+        }
+        
     }
-}
-int main()
-{
-    int n=5;
-    int arr[6] = {5,3,2,6,33};
-    buildHeap(arr,n);
-    insertHeap(68,arr,n++);
-    deleteHeap(arr,n--,0);
-    printArr(arr,0,n);
-    return 0;
-}
+    int heapSort(int *heap,int n)
+    {
+        buildHeap(heap,n);
+
+        for (int i = n-1; i >=0; i--)
+        {
+            swap(heap,0,i);
+            heapify(heap,i,0);
+
+        }
+    }
+    int main()
+    {
+        int n=5;
+        int arr[6] = {5,3,2,6,33};
+        buildHeap(arr,n);
+        insertHeap(68,arr,n++);
+        deleteHeap(arr,n--,0);
+        printArr(arr,0,n);
+        return 0;
+    }
