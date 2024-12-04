6da418edf921d616fb3b8b96f6b618ed5d0ced46
tr1ten <shubhi.blog@gmail.com>
2024-12-04 19:00:00 +0530

    chore: Wednesday 04 December 2024 07:00:00 PM IST solutions

--- 2039C2.cpp
+++ 2039C2.cpp
@@ -24,43 +24,63 @@ inline int hset(int x) {return __lg(x);}
 
 const int MOD = 1e9+7; // change me for god sake look at problem mod
 const int INF = 1e16+5;
+// Function to calculate the number of solutions to ax ≡ b (mod m) with 0 <= x < n
+// Extended Euclidean Algorithm (needed by the count_congruence_solutions function)
+long long extended_gcd(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long x1, y1;
+    long long d = extended_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - a / b * y1;
+    return d;
+}
+long long count_solutions(long long a, long long b, long long m, long long n) {
+    long long g = __gcd(a, m);
+    if (b % g != 0) {
+        return 0; // No solutions
+    }
+
+    long long a_prime = a / g;
+    long long b_prime = b / g;
+    long long m_prime = m / g;
+
+    long long x, y;
+    long long gcd_val = extended_gcd(a_prime, m_prime, x, y); //Reusing extended_gcd to compute inverse
+    if(gcd_val != 1) return 0;
+
+    long long x0 = (x * b_prime) % m_prime;
+    if (x0 < 0) x0 += m_prime;
+
+
+    if (x0 >= n) {
+        return 0;
+    }
+
+    return (n - 1 - x0) / m_prime + 1;
+}
 void testcase(){
     int x,m;
     cin >> x >> m;
     int h = hset(x);
     int ans = 0;
     int mx=  (1LL<<(h+1))-1;
-    int r2 =((int)pow(2LL,h+1LL))%x;
+    int r2 =(int)pow(2,h+1)%x;
     rep(y,0,min(mx+1,m+1) ){
-        // if(y!=0 && ((x^y)%y==0 && (x^y)%x!=0)){
-        //     ans++;
-        // }
-        int have = 0;
-        rep(yy,0,m+1) {
-            if((yy&(mx))==y) have++;
+        if(y!=0 && ((x^y)%y==0 && (x^y)%x!=0)){
+            ans++;
         }
-        cout << y << " have "<<have << endl;
+        int have = 1+((m-y)>>(h+1));
         int z = (x^y);
         int r1 = (x-z%x)%x;
-        if(r1==0){
-
-        }
-        else {
-            if(r2==0 || r1%r2) continue;
-            int r3 =r1/r2;
-            
-        }
+        ans += count_solutions(r2,r1,x,have);
     }
-    int real = 0;
-    rep(y,1,m+1){
-        if((x^y)%y==0 || (x^y)%x==0){
-            // cout << y << endl;
-             real++;}
-    }
-    cout<< real << " " << ans-1 << endl;
-    // assert(real==ans-1);
     put(ans-1);
 }
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
