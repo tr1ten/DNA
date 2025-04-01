e949d5bf0b11e26a3a4e2228fe39dc48b99b7e5f
tr1ten <shubhi.blog@gmail.com>
2025-04-01 19:00:00 +0530

    chore: Tuesday 01 April 2025 07:00:00 PM IST solutions

--- Subarray_Squares.cpp
+++ Subarray_Squares.cpp
@@ -1,164 +1,69 @@
-#include <cstdio>
 #include <bits/stdc++.h>
-
 using namespace std;
-#include "ext/pb_ds/assoc_container.hpp"
-#include "ext/pb_ds/tree_policy.hpp"
-using namespace __gnu_pbds;
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
-template<typename T> 
-using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
-// find_by_order(k)  returns iterator to kth element starting from 0;
-// order_of_key(k) returns count of elements strictly smaller than k;
-// useful defs
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(int x) const { return x ^ RANDOM; }
+/*
+    NOT use unordered map use map
+    NOT use seg tree use fenwick tree
+    THINK before you code
+*/
+
+const int MOD = 1e9+7; // change me for god sake look at problem mod
+const int INF = 1e16+5;
+
+#define ll long long
+const long long inf = 1e15;
+const int N = 1005;
+ll dp_prev[N],dp_cur[N];
+ll i,K,n;
+ll a_pref[N],c_pref[N];
+ll get_cost(int k,int j){
+    return (a_pref[j+1]+K*i)*(c_pref[j+1]-c_pref[k]);
+}
+ll mn_ans;
+void solve_dp(int l,int r,int optl,int optr){
+    if(l>r) return;
+    int j = (l+r)>>1;
+    pair<ll,int> best = {inf,n};
+    for(int k=0;k<=min(j,optr);k++){
+        best = min(best,{ (k? dp_prev[k-1] : 0) + get_cost(k,j),k});
+    }
+    dp_cur[j] = best.first;
+    solve_dp(l,j-1,optl,best.second);
+    solve_dp(j+1,r,best.second,optr);
+}
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums, vector<int>& cost, int k) {
+        n = nums.size();
+        int m=n;
+        K = k;
+        for(int i=0;i<n;i++){
+            a_pref[i+1] = a_pref[i] + nums[i];
+            c_pref[i+1] = c_pref[i] + cost[i];
+        } 
+        mn_ans = inf;   
+        i = 1;
+        for(int j=0;j<n;j++){
+            dp_prev[j] = get_cost(0,j);
+        }
+        mn_ans = dp_prev[n-1];
+        while (i<=n)
+        {
+            i++;
+            solve_dp(0,n-1,0,n-1);
+            mn_ans = min(mn_ans,dp_cur[n-1]);
+            swap(dp_cur,dp_prev);
+        }
+        return mn_ans;
+    }
 };
-// gp_hash_table<int, int, chash> table;
-template <class K, class V>
 
-using ht = gp_hash_table<
-
-    K, V, hash<K>, equal_to<K>, direct_mask_range_hashing<>, linear_probe_fn<>,
-
-    hash_standard_resize_policy<hash_exponential_size_policy<>,
-
-                                hash_load_check_resize_trigger<>, true>>;
-
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-// ht<int, null_type> g;
-
-typedef long long ll; 
-typedef unsigned long long ull; 
-typedef vector<ll> vi;
-typedef vector<vi> vii;
-typedef pair<ll,ll> pi;
-typedef vector<pi> vpi;
-typedef unordered_map<ll,ll,custom_hash> mll;
-#define pb push_back
-#define mp make_pair
-#define ss second
-#define ff first
-#define int long long
-#define rep(i,a,b) for (int i = (a); i < (b); i++)
-#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
-#define trav(a,arr) for (auto& a: (arr))
-#define sz(x) (int)(x).size()
-#define mk_vec(name,sz,value) vi name(sz,value)
-#define mk_mat(name,n,m,value) vii name(n, vi(m, value))
-#define contains(x) find(x) != string::npos
-#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
-#define srv(vec) sort(vec.begin(), vec.end())
-#define all(x) x.begin(), x.end()
-#define less(a,b) a<b
-#define vsum(vec) accumulate(vec.begin(), vec.end(), 0L);
-#define vmax(vec) *max_element(vec.begin(), vec.end());
-#define vmin(vec) *min_element(vec.begin(), vec.end());
-#define pvc(vec) trav(x,vec) cout<<x<<" "; cout<<endl;
-#define put(x) cout<<(x)<<endl;
-#define put2(x,y) cout<<(x)<<" "<<(y)<<endl;
-#define put3(x,y,z) cout<<(x)<<" "<<(y)<<" "<<(z)<<endl;
-#define mod(x) (x + MOD)%MOD
-// debugging
-#define timed(x) {auto start = chrono::steady_clock::now(); x; auto end = chrono::steady_clock::now(); auto diff = end - start; cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;}
-
-
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
-const ll MOD = 1e9+7; // change me for god sake look at problem mod
-const ll INF = 1e18+5;
-
-inline int ctz(ll x) { return __builtin_ctzll(x);}
-inline int clz(ll x) {return __builtin_clzll(x);}
-inline int pc(ll x) {return  __builtin_popcount(x);} 
-inline int hset(ll x) {return __lg(x);}
-void pyn(int x) {put(x?"YES":"NO");}
-// do not use unordered map use mll
-const int N = 3005;
-int dp_prev[N];
-int dp_cur[N];
-int a[N];
-int pa[N];
-// must satisfy quandrangle inequality
-int C(int i,int j) {
-    return (pa[j+1]-pa[i])*(pa[j+1]-pa[i]);
-}
-// find dp[l]...dp[r]
-void compute(int l,int r,int opl,int opr){
-    if(r<l) return;
-    int mid = (l+r) >> 1;
-    pair<int,int> best = {INF,-1};
-    for(int k=opl;k<=min(opr,mid);k++){
-        best = min(best,{(k-1>=0 ? dp_prev[k-1] : 0) + C(k,mid),k}  );
-    }
-    dp_cur[mid] = best.first;
-    int opt = best.second;
-    compute(l,mid-1,opl,opt);
-    compute(mid+1,r,opt,opr);
-}
-
-void testcase(){
-    int n,k;
-    cin >> n>> k;
-    tkv(a,n);
-    rep(i,0,n) {pa[i+1] =a[i]+pa[i]; dp_prev[i] = INF;}
-    rep(i,0,k){
-        compute(0,n-1,0,n-1);
-        rep(i,0,n) dp_prev[i] = dp_cur[i];
-    }
-    put(dp_cur[n-1]);
-    
-
-}
-// driver code
-int32_t main()
+int main(int argc, char const *argv[])
 {
-    ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-    // freopen("input.in","r",stdin);
-    // freopen("output.out","w",stdout);      
-    int T=1;
-    // cin>>T;
-    while(T--) testcase();
-
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n),cos(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+    for(int i=0;i<n;i++) cin >> cos[i];
+    cout << Solution().minimumCost(a,cos,k) << endl;
     return 0;
 }
