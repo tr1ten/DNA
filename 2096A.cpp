19e00bfe17b722cc2ba908e627630df080783926
tr1ten <shubhi.blog@gmail.com>
2025-04-29 19:00:00 +0530

    chore: Tuesday 29 April 2025 07:00:00 PM IST solutions

---
+++ 2096A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ss second
+#define ff first
+#define int long long
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+#define per(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define tkv(vec,sz) rep(i,0,sz) cin>>vec[i]
+#define srv(vec) sort(vec.begin(), vec.end())
+#define all(x) x.begin(), x.end()
+#define pvc(vec) for(auto x:vec) cout<<x<<" "; cout<<endl;
+#define put(x) cout << (x)<< endl;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef pair<int,int> pi;
+inline int ctz(int x) { return __builtin_ctzll(x);}
+inline int pc(int x) {return  __builtin_popcount(x);} 
+inline int hset(int x) {return __lg(x);}
+/*
+    NOT use unordered map use map
+    NOT use seg tree use fenwick tree
+    THINK before you code
+*/
+
+const int MOD = 1e9+7; // change me for god sake look at problem mod
+const int INF = 1e16+5;
+void testcase(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int i =0 ;
+    int nxt = 1;
+    vector<int> a;
+    while(i<n-1){
+        int j = i;
+        vector<int> b;
+        while(s[j]==s[i]) {j++;b.push_back(nxt++);}
+        if(s[i]=='<') {
+            b.push_back(nxt++); reverse(all(b));
+            
+        }
+        a.insert(a.end(),all(b));
+        i = j;
+    }
+    pvc(a);
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);      
+    int T=1;
+    cin>>T;
+    while(T--) testcase();
+
+    return 0;
+}
+
+// 1 2 3 4 5
+// >><>
+// 3>2>1<4>5
